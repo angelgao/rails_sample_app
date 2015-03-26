5714d82b804483cb2b12887fc24d540250a01004
angelgao <angel_gao@live.ca>
2015-03-26 21:10:33 +0000

    Finding adding all the layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'rule/input'
 
-  get 'rule/output'
+  get 'signup' => 'users#new'
 
   root 'static_pages#home'
 
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pagescontact'
   
 
   # The priority is based upon order of creation: first created -> highest priority.
